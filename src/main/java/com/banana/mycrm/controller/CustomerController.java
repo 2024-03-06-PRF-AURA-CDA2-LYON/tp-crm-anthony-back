2538b3d42014573f033e97b209c82fce0928cb18
anthonyluc <anthony.luc69@gmail.com>
2024-10-02 16:41:38 +0200

    Create CustomerRepository

--- src/main/java/com/banana/mycrm/controller/CustomerController.java
+++ src/main/java/com/banana/mycrm/controller/CustomerController.java
@@ -1,5 +1,12 @@
 package com.banana.mycrm.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/users", produces="application/json")
+@CrossOrigin(origins="*")
 public class CustomerController {
 
 }
---
+++ src/main/java/com/banana/mycrm/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.banana.mycrm.repository;
+
+import com.banana.mycrm.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
